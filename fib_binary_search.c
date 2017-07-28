bf2e1518f9d6062963bc1d8dbcf5825d99f6eb60
xingrui <xingrui@gmail.com>
2017-07-28 10:12:20 +0800

    update binary search algorithm(fix bug).

--- fib_binary_search.c
+++ fib_binary_search.c
@@ -8,6 +8,58 @@ int array[] = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
                 267914296, 433494437, 701408733, 1134903170, 1836311903,
               };
 
+int search1(int* array, int length, int num)
+{
+    int low = -1;
+    int high = length;
+
+    while (high - low > 1) {
+        int range_length = high - low;
+        int probe = low + range_length / 2;
+
+        if (array[probe] < num) {
+            low = probe;
+        } else {
+            high = probe;
+        }
+    }
+
+    if (low < length && array[low + 1] == num) {
+        printf("found: index = %d\n", low + 1);
+    } else {
+        printf("not found: index(largest number not greater than input num) = %d \n", low);
+        printf("not found: index(least number greater than input num) = %d \n", low + 1);
+    }
+
+    return 0;
+}
+
+int search2(int* array, int length, int num)
+{
+    int low = -1;
+    int high = length;
+
+    while (high - low > 1) {
+        int length = high - low;
+        int probe = low + length / 2;
+
+        if (array[probe] > num) {
+            high = probe;
+        } else {
+            low = probe;
+        }
+    }
+
+    if (low != -1 && array[low] == num) {
+        printf("found: index = %d\n", low);
+    } else {
+        printf("not found: index(largest number not greater than input num) = %d \n", low);
+        printf("not found: index(least number greater than input num) = %d \n", low + 1);
+    }
+
+    return 0;
+}
+
 int main(void)
 {
     int num;
@@ -16,26 +68,8 @@ int main(void)
         if (num < 0) {
             printf("%d\n", -1);
         } else {
-            int low = -1;
-            int high = sizeof(array) / sizeof(int);
-
-            while (high - low > 1) {
-                int length = high - low;
-                int probe = low + length / 2;
-
-                if (array[probe] < num) {
-                    low = probe;
-                } else {
-                    high = probe;
-                }
-            }
-
-            if (array[low + 1] == num) {
-                printf("found: index = %d\n", low + 1);
-            } else {
-                printf("not found: index(largest number not greater than input num) = %d \n", low);
-                printf("not found: index(least number greater than input num) = %d \n", low + 1);
-            }
+            search1(array, sizeof(array) / sizeof(int), num);
+            search2(array, sizeof(array) / sizeof(int), num);
         }
 
         fflush(stdout);
