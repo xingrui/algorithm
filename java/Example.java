a4c415a3a654e98e29cd8c629d9a0b569898e246
xingrui <xingrui@gmail.com>
2017-08-03 16:16:55 +0800

    update Example.java file.

--- java/Example.java
+++ java/Example.java
@@ -4,14 +4,14 @@ public class Example {
         int s = 3;
         char[] arr = str.toCharArray();
         char[] tmp = new char[s];
-        Combine(arr, 0, arr.length, tmp, s, s);
+        Combine(arr, 0, arr.length - 1, tmp, s, s);
     }
     public static void Combine(char[] arr, int left, int right, char[] tmp, int s, int remain) {
         if (remain == 0) {
             System.out.println(String.valueOf(tmp));
             return;
         }
-        for (int i = left; i < right - remain + 1; ++i) {
+        for (int i = left; i <= right - remain + 1; ++i) {
             tmp[s - remain] = arr[i];
             Combine(arr, i + 1, right, tmp, s, remain - 1);
         }
