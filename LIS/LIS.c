c0962dfe80780beac5553c6a8af1e8f9253831cc
xingrui <xingrui05@gmail.com>
2017-08-13 02:40:58 -0700

    add print array method(but inverse).

--- LIS/LIS.c
+++ LIS/LIS.c
@@ -1,53 +1,85 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int binary_search(int* array, int length, int num) {
+int binary_search(int* array, int length, int num)
+{
     int low = -1;
     int high = length;
+
     while (high - low > 1) {
         int len = high - low;
         int mid = low + len / 2;
+
         if (array[mid] < num) {
             low = mid;
         } else {
             high = mid;
         }
     }
+
     return low + 1;
 }
 
-int LIS(int*array, int length) {
-    int* tmp_array = (int*)malloc(length * sizeof(int)); 
+int LIS(int* array, int length, int print_sub_array)
+{
+    int* tmp_array = (int*)malloc(length * sizeof(int));
+    int* mid_res_array = (int*)malloc(length * sizeof(int) * 3);
     int current_length = 0;
     int i = 0;
+
     for (i = 0; i < length; ++i) {
         int current_num = array[i];
         int search_res = binary_search(tmp_array, current_length, current_num);
         tmp_array[search_res] = array[i];
+        int pre_data = search_res == 0 ? 0 : (tmp_array[search_res - 1]);
+        mid_res_array[i * 3] = search_res;
+        mid_res_array[i * 3 + 1] = array[i];
+        mid_res_array[i * 3 + 2] = pre_data;
+
         if (search_res == current_length) {
             ++current_length;
         }
-        printf("search_res of array[%d]=%d is %d:\n", i, current_num, search_res);
     }
+
+    int to_find_index = current_length - 1;
+    int to_find_data = tmp_array[to_find_index];
+
+    for (i = length - 1; i >= 0; --i) {
+        int index = mid_res_array[i * 3];
+        int cur_data = mid_res_array[i * 3 + 1];
+        int pre_data = mid_res_array[i * 3 + 2];
+
+        if (index == to_find_index && cur_data == to_find_data) {
+            printf("%d\t", cur_data);
+            --to_find_index;
+            to_find_data = pre_data;
+        }
+    }
+
+    printf("\n");
     return current_length;
 }
 
-int test_binary_search(){
-    int array[] = {0,1,2,3,4,5,6,7,8,9,10};
+int test_binary_search()
+{
+    int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int length = sizeof(array) / sizeof(int);
     int i = 0;
+
     for (i = -1; i <= length + 2; ++i) {
-        int loc = binary_search(array, length, i); 
+        int loc = binary_search(array, length, i);
         printf("location of %d is %d\n", i, loc);
     }
+
     return 0;
 }
 
-int main(){
+int main()
+{
     // int array[] = {100, 0, 1, 2, 1000, 2000, 3, 4, 5, 6, 7, 8};
-    int array[] = {1, -1, 2, -3, 4, -5, 6, -7};
+    int array[] = {1, -1, 2, -3, 4, -5, 6, -7, -1, 2, -3, 4, -5, 6, -7};
     int length = sizeof(array) / sizeof(int);
-    int lis_len = LIS(array, length);
+    int lis_len = LIS(array, length, 0);
     printf("LIS len res = %d\n", lis_len);
     return 0;
 }
