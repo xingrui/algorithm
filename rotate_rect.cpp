57432bca007a910810447f4579d7b3539c9cae63
xingrui <xingrui05@gmail.com>
2017-08-23 06:55:19 +0800

    1.using std::vector instead of raw pointer. 2.change some variables and functions to static.

--- rotate_rect.cpp
+++ rotate_rect.cpp
@@ -1,22 +1,14 @@
 #include<iostream>
-#include<cstring>
+#include<vector>
 class RotateRect {
 private:
     int m_row;
     int m_column;
-    int* m_pData;
-    RotateRect(const RotateRect&);
-    RotateRect& operator=(const RotateRect&);
+    std::vector<int> m_vectData;
+    static const int STEP_I[4];
+    static const int STEP_J[4];
 public:
-    RotateRect(int row, int column): m_row(row), m_column(column), m_pData(NULL) {
-        int size = m_row * m_column;
-        m_pData = new int[size];
-        memset(m_pData, 0, size * sizeof(int));
-    }
-    ~RotateRect() {
-        delete[] m_pData;
-        m_pData = NULL;
-    }
+    RotateRect(int row, int column): m_row(row), m_column(column), m_vectData(m_row * m_column, 0) {}
     void printArray() const {
         for (int i = 0; i < m_row; ++i) {
             for (int j = 0; j < m_column; ++j) {
@@ -25,7 +17,7 @@ public:
             std::cout << std::endl;
         }
     }
-    int fillRect() {
+    void fillRect() {
         int i = 0, j = 0;
         // 0:> 1:V 2:< 3:^
         int direction = 0;
@@ -38,10 +30,15 @@ public:
         }
     }
 private:
-    int nextDirection(int direction) const {
+    static int nextDirection(int direction) {
         return (direction + 1) % 4;
     }
-    int try_move(int& i, int& j, int direction) {
+    static void move(int i, int j, int& res_i, int& res_j, int direction) {
+        res_i = i + STEP_I[direction];
+        res_j = j + STEP_J[direction];
+    }
+private:
+    int try_move(int& i, int& j, int direction) const {
         int next_i, next_j;
         move(i, j, next_i, next_j, direction);
         if (checkValid(next_i, next_j)) {
@@ -51,12 +48,6 @@ private:
         }
         return 0;
     }
-    int move(int i, int j, int& res_i, int& res_j, int direction) const {
-        int step_i[4] = {0, 1, 0, -1};
-        int step_j[4] = {1, 0, -1 , 0};
-        res_i = i + step_i[direction];
-        res_j = j + step_j[direction];
-    }
     bool checkValid(int i, int j) const {
         if (i < 0 || i >= m_row) {return false;}
         if (j < 0 || j >= m_column) {return false;}
@@ -64,27 +55,25 @@ private:
         return true;
     }
     int getData(int i, int j) const {
-        return m_pData[i * m_column + j];
+        return m_vectData[i * m_column + j];
     }
-    int setData(int i, int j, int val) {
-        m_pData[i * m_column + j] = val;
+    void setData(int i, int j, int val) {
+        m_vectData[i * m_column + j] = val;
     }
 };
-
+const int RotateRect::STEP_I[4] = {0, 1, 0, -1};
+const int RotateRect::STEP_J[4] = {1, 0, -1 , 0};
 int getRow(int sum)
 {
-    int res = 0;
+    int res = 1;
     for (int i = 1; i * i <= sum; ++i) {
-        if (sum % i == 0) {
-            res = i;
-        }
+        if (sum % i == 0) {res = i;}
     }
     return res;
 }
-
 int main()
 {
-    int sum = 30;
+    int sum = 35;
     int row = getRow(sum);
     int column = sum / row;
     RotateRect r(row, column);
