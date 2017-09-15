15f0f5a6bccc9089fc5bf2ee7c939dd9b5266057
xingrui <xingrui05@gmail.com>
2017-09-15 14:13:33 +0800

    rename file.

--- examples/min_distance_tree.cpp
+++ examples/min_distance_tree.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <algorithm>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::max;
 
 struct NODE
 {
 	NODE *pLeft;
 	NODE *pRight;
+    NODE():pLeft(NULL), pRight(NULL) {}
 };
 
 struct RESULT
@@ -29,4 +33,18 @@ RESULT GetMaximumDistance(NODE* root)
 	result.nMaxDepth = max(lhs.nMaxDepth + 1, rhs.nMaxDepth + 1);
 	result.nMaxDistance = max(max(lhs.nMaxDistance, rhs.nMaxDistance), lhs.nMaxDepth + rhs.nMaxDepth + 2);
 	return result;
+}
+
+int main() {
+    NODE* root = new NODE;
+    root->pLeft = new NODE;
+    root->pLeft->pLeft = new NODE;
+    root->pLeft->pRight = new NODE;
+    root->pLeft->pRight->pLeft = new NODE;
+    root->pRight = new NODE;
+    root->pRight->pLeft = new NODE;
+    root->pRight->pRight = new NODE;
+    root->pRight->pRight->pLeft = new NODE;
+    RESULT res = GetMaximumDistance(root);
+    std::cout << res.nMaxDistance << std::endl;
 }
