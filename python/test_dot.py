1f656b6a20c79cfdc972ca43536d3d0feaaee7ee
xingrui <xingrui05@gmail.com>
2017-09-23 09:27:04 +0800

    add mat dot result demo.

--- python/test_dot.py
+++ python/test_dot.py
@@ -3,5 +3,10 @@ from numpy import *
 a = array([[1,2,3],[4,5,6],[7,8,9]])
 b = array([1,2,3])
 
-print dot(a,b) # b as (1,3) array
-print dot(b,a) # b as (3,1) array
+print dot(a,b), dot(a,b).shape # b as (1,3) array
+print dot(b,a), dot(b,a).shape # b as (3,1) array
+
+a = mat(a)
+
+print dot(a,b), dot(a,b).shape # b as (1,3) array
+print dot(b,a), dot(b,a).shape # b as (3,1) array
